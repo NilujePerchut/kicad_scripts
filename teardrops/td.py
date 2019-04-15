36eee96fff9819fc3be4e9191195a144b55c3b7d
NilujePerchut <debarge.cedric@gmail.com>
2019-04-15 21:08:31 +0200

    Bump to version 2.4.3

--- teardrops/td.py
+++ teardrops/td.py
@@ -11,7 +11,7 @@ from math import cos, acos, sin, asin, tan, atan2, sqrt
 from pcbnew import VIA, ToMM, TRACK, FromMM, wxPoint, GetBoard, ZONE_CONTAINER
 from pcbnew import PAD_ATTRIB_STANDARD, ZONE_FILLER
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 
 ToUnits = ToMM
 FromUnits = FromMM
