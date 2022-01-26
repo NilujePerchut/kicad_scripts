c4323645373b5ca598938029b0af9d789c4c89bd
mitxela <mitxela@mitxela.com>
2022-01-26 19:33:55 +0000

    Make GUI window bigger
    the number inputs are rendering weirdly and pushing the other options out of the window for some reason

--- teardrops/teardrop_gui.py
+++ teardrops/teardrop_gui.py
@@ -17,7 +17,7 @@ import wx.xrc
 class teardrop_gui ( wx.Dialog ):
 
 	def __init__( self, parent ):
-		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Teardrops", pos = wx.DefaultPosition, size = wx.Size( 450,368 ), style = wx.CAPTION|wx.CLOSE_BOX|wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER )
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Teardrops", pos = wx.DefaultPosition, size = wx.Size( 450,415 ), style = wx.CAPTION|wx.CLOSE_BOX|wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER )
 
 		#self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 		import sys
